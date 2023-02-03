b63a2d6c2f653e8161d18c1e3477e71fa92062d7
Daria <darya.piankova@yandex.ru>
2023-02-03 17:13:18 +0500

    task3

--- Tsk1.cs
+++ Tsk1.cs
@@ -1,18 +1,7 @@
-﻿Console.WriteLine("Input 3 numbers\n");
+﻿Console.WriteLine("Input a number\n");
 int a = Convert.ToInt32(Console.ReadLine());
-int b = Convert.ToInt32(Console.ReadLine());
-int c = Convert.ToInt32(Console.ReadLine());
-if (a>b){
-    if(a>c){
-        Console.WriteLine($"{a} is the biggest number\n");
-    }else if (b>c){
-        Console.WriteLine($"{b} is the biggest number\n");
-    }else if(c>b){
-        Console.WriteLine($"{c} is the biggest number\n");
-    }
-}else if(b>c){
-    Console.WriteLine($"{b} is the biggest number\n");
-}else if(c>b){
-    Console.WriteLine($"{c} is the biggest number\n");
-
-
+if (a%2==0){
+    Console.WriteLine($"{a} is even integer\n");
+}else{
+    Console.WriteLine($"{a} is odd integer\n"); 
+}
