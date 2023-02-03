23ec140ddbf4ba798a5f342aeb4396e0e762e22f
Daria <darya.piankova@yandex.ru>
2023-02-03 17:20:42 +0500

    task4

--- Tsk1.cs
+++ Tsk1.cs
@@ -1,5 +1,5 @@
 ﻿Console.WriteLine("Input an integer\n");
 int a = Convert.ToInt32(Console.ReadLine());
 for (int i=2; i<=a; i+=2){
-    Console.WriteLine("{i} ");
+    Console.WriteLine($"{i} ");
 }
