7d2d90d6f29a4aaa815f07b41ce295b909f09929
Daria <darya.piankova@yandex.ru>
2023-02-03 17:05:24 +0500

    task2

--- Tsk1.cs
+++ Tsk1.cs
@@ -1,13 +1,18 @@
-﻿
-
-Console.WriteLine("Input 2 numbers\n");
+﻿Console.WriteLine("Input 3 numbers\n");
 int a = Convert.ToInt32(Console.ReadLine());
 int b = Convert.ToInt32(Console.ReadLine());
-if(a>b){
-    Console.WriteLine($"{a}>{b}\n");
-}else if(a<b){
-    Console.WriteLine($"{a}<{b}\n");
-}else if(a==b){
-    Console.WriteLine($"{a}=={b}\n");
-}
+int c = Convert.ToInt32(Console.ReadLine());
+if (a>b){
+    if(a>c){
+        Console.WriteLine($"{a} is the biggest number\n");
+    }else if (b>c){
+        Console.WriteLine($"{b} is the biggest number\n");
+    }else if(c>b){
+        Console.WriteLine($"{c} is the biggest number\n");
+    }
+}else if(b>c){
+    Console.WriteLine($"{b} is the biggest number\n");
+}else if(c>b){
+    Console.WriteLine($"{c} is the biggest number\n");
+
 
