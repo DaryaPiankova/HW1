26b9fa31bab3bc7f083920fd67f423222ab504a4
Daria <darya.piankova@yandex.ru>
2023-02-03 17:03:26 +0500

    init

---
+++ Tsk1.cs
@@ -0,0 +1,13 @@
+﻿
+
+Console.WriteLine("Input 2 numbers\n");
+int a = Convert.ToInt32(Console.ReadLine());
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b){
+    Console.WriteLine($"{a}>{b}\n");
+}else if(a<b){
+    Console.WriteLine($"{a}<{b}\n");
+}else if(a==b){
+    Console.WriteLine($"{a}=={b}\n");
+}
+
